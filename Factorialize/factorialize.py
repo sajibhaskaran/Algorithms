e9b105d199546f8abab4cbf811961d7b26f3e4ed
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-01-30 22:51:25 -0800

    initial commit

--- Factorialize/factorialize.py
+++ Factorialize/factorialize.py
@@ -1,14 +1,27 @@
-
+# Purpose: Factorialize a Number
+#          Return the factorial of the provided integer.
+# Author : Saji Bhaskaran
 
 
 def factorialize(number):
     result = 1
     if number == 0:
-        result = 1
+        return result
     else:
         for i in range(1,number+1):
             result *= i
     return result
 
-print(factorialize(15))
+# Refactored...
+
+def factor(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+print(factorialize(10))
+print(factorialize(0))
+print(factor(10))
+print(factor(0))
     
