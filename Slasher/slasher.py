5fe90b6b1bc34e2b6dca20c7ae9e21193f8dd3d5
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-02-06 20:54:55 -0800

    modified

--- Slasher/slasher.py
+++ Slasher/slasher.py
@@ -1,14 +1,17 @@
-
+# Purpose : Return the remaining elements of a list after chopping off
+#         : n elements from the head.
+#
+# Author  : Saji Bhaskaran
 
 
 def slasher(lst, num):
-    s = lst[num:]
-    print(s)
+    slashed = lst[num:]
+    print(slashed)
+
 
 slasher([1, 2, 3], 4)
-
 slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4], 5)
-slasher(["burgers", "fries", "shake"], 6)
+
 
 
     
