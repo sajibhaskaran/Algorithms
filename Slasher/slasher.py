7ac10d8ad530db178207604fda50f319725b4418
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-02-06 20:52:11 -0800

    modified

---
+++ Slasher/slasher.py
@@ -0,0 +1,14 @@
+
+
+
+def slasher(lst, num):
+    s = lst[num:]
+    print(s)
+
+slasher([1, 2, 3], 4)
+
+slasher([1, 2, "chicken", 3, "potatoes", "cheese", 4], 5)
+slasher(["burgers", "fries", "shake"], 6)
+
+
+    
