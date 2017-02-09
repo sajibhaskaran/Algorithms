13059821fc0f8493bd24d9c9f9956aecb72e724a
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-02-09 13:22:56 -0800

    modified

--- mutation.py
+++ mutation.py
@@ -2,11 +2,21 @@
 
 def mutation(lst):
     x = [i.lower() for i in lst]
+    for i in x[0]:
+        a = x[1].find(i)
+        if a == -1:
+            return False
+            break;
+    return True
+           
+    
+                
         
 
-    print(x)
+    #print(x)
 
+print(mutation(["hello", "Hello"]))
+print(mutation(["hello", "hey"]))
+print(mutation(["Mary", "Aarmy"]))
 
-mutation(["hello", "hey"])
-mutation(["hello", "Hello"])
     
