7472b48940d0366511608faa20b3ce6b1cb5e051
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-02-17 10:07:33 -0800

    modified

--- cipher_13.py
+++ cipher_13.py
@@ -1,3 +1,8 @@
+# Purpose : Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+#         : All letters will be uppercase. Do not transform any non-alphabetic
+#         : character (i.e. spaces, punctuation), but do pass them on.
+#         :
+# Author  : Saji Bhaskaran
 
 
 def cipher_13(str):
