d7f941cd1e5d4bddd9ec1474539cba3020c8ee68
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-02-16 19:08:04 -0800

    modified

---
+++ GetIndex/get_index.py
@@ -0,0 +1,16 @@
+
+
+
+def get_index(lst, num):
+    lst.append(num)
+    lst.sort()
+    print(lst.index(num))
+
+
+
+
+get_index([10, 20, 30, 40, 50], 35)
+get_index([10, 20, 30, 40, 50], 30)
+get_index([3, 10, 5], 2)
+
+
