bed14ceb699af1cc5adb1a61d6636c9919dbe48a
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-02-17 10:04:37 -0800

    modified

--- cipher_13.py
+++ cipher_13.py
@@ -4,18 +4,19 @@ def cipher_13(str):
     lst = [ord(c) for c in str]
     new_lst =[]
     for i in lst:        
-        if 65 < i < 90:
-            i = 65 + (i -90)
-        new_lst.append(i+32)
-    new_lst = [chr(c) for c in new_lst]
-            
-
-    
+        if 64 < i < 91:
+            if (i + 13) > 90:
+                i = 64 + ((i + 13)-90)
+            else:
+                i = i + 13
+        new_lst.append(i)                
+          
+    new_lst = ''.join([chr(c) for c in new_lst])          
+       
         
-    
-    print(lst)
     print(new_lst)
 
 
 
-cipher_13("SAJI BHASKARAN")
+cipher_13("SERR YBIR?")
+cipher_13("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK.")
