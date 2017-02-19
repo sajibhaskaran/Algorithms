229e11e4327569d1865b6e91036fb42fee814243
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-02-19 10:01:19 -0800

    modified

--- diff_array.py
+++ diff_array.py
@@ -1,6 +1,10 @@
+# Purpose : Compare two arrays and return a new array with any items only found
+#         : in one of the two given arrays, but not both.
+#         : In other words, return the symmetric difference of the two arrays.
+#
+# Author  : Saji Bhaskaran
 
-
-def diff(lst1, lst2):
+def diff_list(lst1, lst2):
     result = []
 
     for i in range(len(lst1)):        
@@ -11,11 +15,10 @@ def diff(lst1, lst2):
     for i in range(len(lst2)):        
         if not lst2[i] in lst1:
             
-            result.append(lst2[i])
-                  
-          
-
-        
+            result.append(lst2[i])                 
     print(result)
 
-diff([1, 2, 3, 5], [1, 2, 3, 4, 5])
+
+diff_list([1, 2, 3, 5], [1, 2, 3, 4, 5])
+diff_list(["andesite", "grass", "dirt", "pink wool", "dead shrub"],
+          ["diorite", "andesite", "grass", "dirt", "dead shrub"])
