d9fce5e245faf7b466731b39ccd607d2d88cfe3c
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-02-23 20:04:17 -0800

    modified

--- DNA/dna_pair.py
+++ DNA/dna_pair.py
@@ -1,12 +1,11 @@
+# Purpose : The DNA strand is missing the pairing element.
+#         : Take each character, get its pair, and return the results as a 2d array.
+#         : Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+# Author  : Saji Bhaskaran
 
 
-def pair(string):
-    match = {
-        "A" : "T",
-        "T" : "A",
-        "C" : "G",
-        "G" : "C"
-        }
+def pair_element(string):
+    match = {"A" : "T","T" : "A","C" : "G","G" : "C"}
     result = []
 
     for c in string:
@@ -16,4 +15,5 @@ def pair(string):
     print(result)
 
 
-pair("GCG")
+pair_element("GCG")
+pair_element("TTGAG")
