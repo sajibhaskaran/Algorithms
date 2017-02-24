20794ff0ce0b9b635978457b96af76a370433639
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-02-23 20:48:58 -0800

    modified

--- missing_letters.py
+++ missing_letters.py
@@ -1,15 +1,20 @@
-
+# Purpose : Find the missing letter in the passed letter range and return it.
+#        : If all letters are present in the range, return undefined.
+#
+# Author : Saji Bhaskaran
 
 def missing_letter(string):
-    word = ''
     start = ord(string[0])
     end = ord(string[-1])
+    
     for i in range(start, end+1):
         if string.find(chr(i)) == -1:
             return chr(i)
-    return "undefined"   
+    return "undefined"
+    
         
         
 
     
-print(missing_letter('abc'))
+print(missing_letter('abd'))
+print(missing_letter('abcdefghjklmno'))
